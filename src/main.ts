d3a4ab7a821ca3a368011059546a9857830b48a1
Ali Muhammad <81983704+Ali-muhammed7040@users.noreply.github.com>
2024-10-09 03:40:32 +0500

    chnages

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { LoggerErrorInterceptor, Logger as PinoLogger } from 'nestjs-pino';
 import { AppModule } from './app.module';
+import { SanitizePipe } from './shared/pipes/sanitize.pipe';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(PinoLogger));
 
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
-  // app.useGlobalGuards(new JwtAuthGuard());
-  // app.useGlobalPipes(new SanitizePipe());
+  app.useGlobalGuards(new JwtAuthGuard());
+  app.useGlobalPipes(new SanitizePipe());
   await app.listen(3000);
 }
 bootstrap();
